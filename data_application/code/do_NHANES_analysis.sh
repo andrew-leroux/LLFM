9e390ce9ff9f20a2ff908abddb7c8966b334f168
Andrew Leroux <andrewleroux@andrews-imac.lan>
2020-10-26 13:39:27 -0600

    bash script

--- data_application/code/do_NHANES_analysis.sh
+++ data_application/code/do_NHANES_analysis.sh
@@ -5,4 +5,4 @@
 #$ -l mem_free=1G,h_vmem=1G
 #$ -t 1-1000
 module load conda_R
-Rscript LLFM_NHANES_manuscript_cluster.R $SGE_TASK_ID $1000 $20
+Rscript LLFM_NHANES_manuscript_cluster.R $SGE_TASK_ID 1000 20
